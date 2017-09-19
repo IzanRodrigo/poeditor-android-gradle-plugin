4939af25302340d93e64fbfd45293b3cbe706910
IzanRodrigo <izan.rmm@gmail.com>
2017-09-19 15:16:42 +0200

    Sort entries.

--- src/main/groovy/com/bq/gradle/XMLCleaner.groovy
+++ src/main/groovy/com/bq/gradle/XMLCleaner.groovy
@@ -1,36 +1,43 @@
 package com.bq.gradle
 
 public class XMLCleaner {
-    public static String clean(String source) {
-        def matcher = source =~ '<string name="(.*)">(.*)</string>'
-        def cleanXml = new StringBuilder("<resources>")
+   public static String clean(String source) {
+      def matcher = source =~ '<string name="(.*)">(.*)</string>'
 
-        while (matcher.find()) {
-            def name = matcher.group(1)
-            def value = matcher.group(2)
-            def entry = cleanEntry(name, value)
-            cleanXml.append("\n\t" + entry)
-        }
+      def entries = new ArrayList<String>()
+      while (matcher.find()) {
+         def name = matcher.group(1)
+         def value = matcher.group(2)
+         def entry = cleanEntry(name, value)
+         entries.add(entry)
+      }
+      Collections.sort(entries)
 
-        cleanXml.append("\n</resources>")
+      def cleanXml = new StringBuilder("<resources>")
+      for (entry in entries) {
+         cleanXml.append("\n\t" + entry)
+      }
+      cleanXml.append("\n</resources>")
 
-        return cleanXml.toString()
-    }
+      return cleanXml.toString()
+   }
 
-    private static String cleanEntry(String name, String value) {
-        def cleanName = cleanName(name)
-        def cleanValue = cleanValue(value)
-        return '<string name="' + cleanName + '">' + cleanValue + '</string>'
-    }
+   private static String cleanEntry(String name, String value) {
+      def cleanName = cleanName(name)
+      def cleanValue = cleanValue(value)
+      return '<string name="' + cleanName + '">' + cleanValue + '</string>'
+   }
 
-    private static String cleanName(String source) {
-        return source.replaceAll("\\+", "_")
-                .toLowerCase()
-    }
+   private static String cleanName(String source) {
+      return source.replaceAll("\\+", "_")
+            .toLowerCase()
+   }
 
-    private static String cleanValue(String source) {
-        return source.replaceAll("\\...", "…")
-                .replaceAll("%%@", "%s")
-                .replaceAll("%@", "%s")
-    }
+   private static String cleanValue(String source) {
+      return source.replaceAll("\\...", "…")
+            .replaceAll('"(?i)Ok"', "@android:string/ok")
+            .replaceAll('"(?i)Cancel"', "@android:string/cancel")
+            .replaceAll("%%@", "%s")
+            .replaceAll("%@", "%s")
+   }
 }
